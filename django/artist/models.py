25ed739d70ad3f325f2b9d422f6551c025e7d8a8
seokjae hong <hsj2334@gmail.com>
2018-02-06 17:08:08 +0900

    data migration

--- django/artist/models.py
+++ django/artist/models.py
@@ -1,26 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
 class Artist(models.Model):
-    BLODD_TYPE_A = 'a'
-    BLODD_TYPE_B = 'b'
-    BLODD_TYPE_O = 'o'
-    BLODD_TYPE_AB = 'c'
-    BLODD_TYPE_OTHER ='x'
-    CHOICE_BLOOD_TYPE = (
-        (BLODD_TYPE_A,'A형'),
-        (BLODD_TYPE_B,'B형'),
-        (BLODD_TYPE_AB,'AB형'),
-        (BLODD_TYPE_O,'O형'),
-        (BLODD_TYPE_OTHER,'기타'),
+    BLOOD_TYPE_A = 'a'
+    BLOOD_TYPE_B = 'b'
+    BLOOD_TYPE_O = 'o'
+    BLOOD_TYPE_AB = 'c'
+    BLOOD_TYPE_OTHER = 'x'
+    CHOICES_BLOOD_TYPE = (
+        (BLOOD_TYPE_A, 'A형'),
+        (BLOOD_TYPE_B, 'B형'),
+        (BLOOD_TYPE_O, 'O형'),
+        (BLOOD_TYPE_AB, 'AB형'),
+        (BLOOD_TYPE_OTHER, '기타'),
     )
     img_profile = models.ImageField(
         '프로필 이미지',
         upload_to='artist',
+        blank=True,
     )
     name = models.CharField(
         '이름',
-        max_length=50
+        max_length=50,
     )
     real_name = models.CharField(
         '본명',
@@ -32,7 +33,7 @@ class Artist(models.Model):
         max_length=50,
         blank=True,
     )
-    birth_date = models.DateTimeField(
+    birth_date = models.DateField(
         '생년월일',
         blank=True,
         null=True,
@@ -40,11 +41,16 @@ class Artist(models.Model):
     constellation = models.CharField(
         '별자리',
         max_length=30,
+        blank=True,
     )
-    broodtype = models.CharField(
+    blood_type = models.CharField(
         '혈액형',
         max_length=1,
-        choices=CHOICE_BLOOD_TYPE,
+        choices=CHOICES_BLOOD_TYPE,
+        blank=True,
+    )
+    intro = models.TextField(
+        '소개',
         blank=True,
     )
 
