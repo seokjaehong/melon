fb543a906c686fd7a3833fc49a03b172cda2e36c
seokjae hong <hsj2334@gmail.com>
2018-02-20 11:25:55 +0900

    song함수추가

--- django/song/urls.py
+++ django/song/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name ='song'
 
 urlpatterns =[
-    path('',views.song_list,name='song-list'),
-    path('search/',views.song_search, name='song-search'),
+    path('', views.song_list, name='song-list'),
+    path('search/', views.song_search, name='song-search'),
 ]
